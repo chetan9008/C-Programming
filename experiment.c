1a5e4015a5a25b0c4d951f8e39545ad5063a84ca
chetan <chetankumar70174577@gmail.com>
2023-09-01 13:13:22 +0530

    some changes

--- experiment.c
+++ experiment.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 #include<math.h>
+float differentiate(float x)
+{
+    return(cos(x));
+}
+
 int main()
 {
-    float num = .25423333;
-    float num1 = .254244444;
-    num = floor(10000 * num) / 10000;
-    num1 = floor(10000 * num1) / 10000;
-    if(num == num1)
-    {
-        printf("yes");
-    }
-    else
-    {
-        printf("no");
-    }
+    printf("%f",(.5+sin(1)));
 }
